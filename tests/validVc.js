1a21e1e7b62290e2c5fdc44fb998bb7128ffaff6
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-06-26 09:00:07 -0400

    Add ecdsa-2019 p-256 issuer tests.

---
+++ tests/validVc.js
@@ -0,0 +1,12 @@
+/*!
+ * Copyright 2023 Digital Bazaar, Inc. All Rights Reserved
+ */
+export const validVc = {
+  '@context': ['https://www.w3.org/2018/credentials/v1'],
+  id: 'urn:uuid:86294362-4254-4f36-854f-3952fe42555d',
+  type: ['VerifiableCredential'],
+  issuer: 'did:key:z6MksNZwi2r6Qxjt3MYLrrZ44gs2fauzgv1dk4E372bNVjtc',
+  credentialSubject: {
+    id: 'did:key:z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b'
+  }
+};
