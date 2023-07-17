a768aeb2874421be2254feb95ecd8f6e048a2804
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-07-17 13:59:43 -0400

    Add `issuanceDate` to initial vc.

--- tests/helpers.js
+++ tests/helpers.js
@@ -4,11 +4,19 @@
 import {klona} from 'klona';
 import {v4 as uuidv4} from 'uuid';
 
+// Javascript's default ISO timestamp contains milliseconds.
+// This lops off the MS part of the UTC RFC3339 TimeStamp and replaces
+// it with a terminal Z.
+export const ISOTimeStamp = ({date = new Date()} = {}) => {
+  return date.toISOString().replace(/\.\d+Z$/, 'Z');
+};
+
 export const createInitialVc = async ({issuer, vc}) => {
   const {settings: {id: issuerId, options}} = issuer;
   const credential = klona(vc);
   credential.id = `urn:uuid:${uuidv4()}`;
   credential.issuer = issuerId;
+  credential.issuanceDate = ISOTimeStamp();
   const body = {credential, options};
   const {data, error} = await issuer.post({json: body});
   if(error) {
