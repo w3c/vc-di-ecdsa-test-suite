feae0dfc81053ae97db852e282395e6e9588c4bb
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-07-20 17:09:16 -0400

    Update `didKeyResolver` to resolve DID keys with `z6Mk` multibaseMultikeyHeader.

--- tests/didResolver.js
+++ tests/didResolver.js
@@ -6,8 +6,17 @@ import * as EcdsaMultikey from '@digitalbazaar/ecdsa-multikey';
 import {
   multibaseMultikeyHeaderP256, multibaseMultikeyHeaderP384
 } from './helpers.js';
+import {Ed25519VerificationKey2020} from
+  '@digitalbazaar/ed25519-verification-key-2020';
 
 const didKeyDriver = didMethodKey.driver();
+
+// FIXME: Remove this once API catalog has been updated to issue
+// VC with ECDSA signature type proof.
+didKeyDriver.use({
+  multibaseMultikeyHeader: 'z6Mk',
+  fromMultibase: Ed25519VerificationKey2020.from
+});
 
 didKeyDriver.use({
   multibaseMultikeyHeader: multibaseMultikeyHeaderP256,
