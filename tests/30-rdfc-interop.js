b25929a21bba014fc9cc7733e93dcd5748f878c4
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-11-19 19:51:39 -0500

    Refactor interop tests.

--- tests/30-rdfc-interop.js
+++ tests/30-rdfc-interop.js
@@ -17,37 +17,86 @@ const {
   match: verifierMatches
 } = endpoints.filterByTag({tags: [tag], property: 'verifiers'});
 
-describe.skip('ecdsa-rdfc-2019 (interop)', function() {
+describe('ecdsa-rdfc-2019 (interop)', function() {
   // this will tell the report
   // to make an interop matrix with this suite
   this.matrix = true;
   this.report = true;
-  this.implemented = [...verifierMatches.keys()];
+  this.implemented = [];
   this.rowLabel = 'Issuer';
   this.columnLabel = 'Verifier';
+
+  const issuers = [];
   for(const [issuerName, {endpoints}] of issuerMatches) {
-    let issuedVc;
-    before(async function() {
-      const [issuer] = endpoints;
-      issuedVc = await createInitialVc({issuer, vc});
-    });
-    for(const [verifierName, {endpoints}] of verifierMatches) {
-      const [verifier] = endpoints;
-      it(`${verifierName} should verify ${issuerName}`, async function() {
-        this.test.cell = {rowId: issuerName, columnId: verifierName};
-        const body = {
-          verifiableCredential: issuedVc,
-          options: {
-            checks: ['proof']
-          }
-        };
-        const {result, error} = await verifier.post({json: body});
-        should.not.exist(error, 'Expected verifier to not error.');
-        should.exist(result, 'Expected result from verifier.');
-        should.exist(result.status, 'Expected verifier to return an HTTP' +
-          'status code');
-        result.status.should.equal(200, 'Expected HTTP status code to be 200.');
+    for(const issuerEndpoint of endpoints) {
+      const {supportedEcdsaKeyTypes} = issuerEndpoint.settings;
+      const issuerDisplayName =
+        `${issuerName}: ${supportedEcdsaKeyTypes.join(', ')}`;
+      for(const issuerSupportedEcdsaKeyType of supportedEcdsaKeyTypes) {
+        issuers.push({
+          issuerDisplayName,
+          issuerSupportedEcdsaKeyType,
+          issuerEndpoint
+        });
+      }
+    }
+  }
+
+  const verifiers = [];
+  for(const [verifierName, {endpoints}] of verifierMatches) {
+    for(const verifierEndpoint of endpoints) {
+      const {supportedEcdsaKeyTypes} = verifierEndpoint.settings;
+      const verifierDisplayName =
+        `${verifierName}: ${supportedEcdsaKeyTypes.join(', ')}`;
+      verifiers.push({
+        verifierDisplayName,
+        verifierSupportedEcdsaKeyTypes: supportedEcdsaKeyTypes,
+        verifierEndpoint
       });
+      // add verifiers' names for reporting
+      this.implemented.push(verifierDisplayName);
+    }
+  }
+
+  for(const {
+    issuerDisplayName, issuerSupportedEcdsaKeyType, issuerEndpoint
+  } of issuers) {
+    for(const {
+      verifierDisplayName, verifierSupportedEcdsaKeyTypes, verifierEndpoint
+    } of verifiers) {
+      if(
+        !verifierSupportedEcdsaKeyTypes.includes(issuerSupportedEcdsaKeyType)
+      ) {
+        // If the issuer keyType is not supported by the verifier
+        // we skip that case
+        continue;
+      }
+      let issuedVc;
+      before(async function() {
+        issuedVc = await createInitialVc({issuer: issuerEndpoint, vc});
+      });
+      it(`'${verifierDisplayName}' should verify '${issuerDisplayName}'`,
+        async function() {
+          this.test.cell = {
+            rowId: issuerDisplayName,
+            columnId: verifierDisplayName
+          };
+
+          const body = {
+            verifiableCredential: issuedVc,
+            options: {
+              checks: ['proof']
+            }
+          };
+
+          const {result, error} = await verifierEndpoint.post({json: body});
+          should.not.exist(error, 'Expected verifier to not error.');
+          should.exist(result, 'Expected result from verifier.');
+          should.exist(result.status, 'Expected verifier to return an HTTP' +
+            'status code');
+          result.status.should.equal(
+            200, 'Expected HTTP status code to be 200.');
+        });
     }
   }
 });
