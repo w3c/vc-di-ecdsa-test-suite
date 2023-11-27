18c90bb38074b4c2ba86f77431761803d1ac280d
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-11-26 19:12:27 -0500

    Update test valid vc.

--- tests/validVc.js
+++ tests/validVc.js
@@ -2,11 +2,18 @@
  * Copyright 2023 Digital Bazaar, Inc. All Rights Reserved
  */
 export const validVc = {
-  '@context': ['https://www.w3.org/2018/credentials/v1'],
-  id: 'urn:uuid:86294362-4254-4f36-854f-3952fe42555d',
-  type: ['VerifiableCredential'],
-  issuer: 'did:key:z6MksNZwi2r6Qxjt3MYLrrZ44gs2fauzgv1dk4E372bNVjtc',
+  '@context': [
+    'https://www.w3.org/2018/credentials/v1',
+    {
+      '@protected': true,
+      AlumniCredential: 'urn:example:AlumniCredential',
+      alumniOf: 'https://schema.org#alumniOf'
+    }
+  ],
+  id: 'urn:uuid:98c5cffc-efa2-43e3-99f5-01e8ef404be0',
+  type: ['VerifiableCredential', 'AlumniCredential'],
   credentialSubject: {
-    id: 'did:key:z6MktKwz7Ge1Yxzr4JHavN33wiwa8y81QdcMRLXQsrH9T53b'
+    id: 'urn:uuid:d58b2365-0951-4373-96c8-e886d61829f2',
+    alumniOf: 'Example University'
   }
 };
