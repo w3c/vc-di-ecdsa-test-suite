5d2a5076d1017be9e28299ee6dd0a0db39cbba45
Andrew Jones <ajones@digitalbazaar.com>
2024-02-02 21:27:09 +0000

    Start generator.

---
+++ tests/vc-generator/index.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright 2023 Digital Bazaar, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+import * as vc from '@digitalbazaar/vc';
+import {validVc} from '../mock-data.js';
+import klona from 'klona';
