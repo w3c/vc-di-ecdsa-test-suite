fa22fbd6dbd77e9295989a56ce55567330bb81c5
Andrew Jones <ajones@digitalbazaar.com>
2024-02-06 14:44:02 +0000

    Add vcCache and initial test gen.

--- tests/vc-generator/index.js
+++ tests/vc-generator/index.js
@@ -5,4 +5,11 @@
 
 import * as vc from '@digitalbazaar/vc';
 import {validVc} from '../mock-data.js';
+import {config, require} from '../helpers.js';
 import klona from 'klona';
+
+const vcCache = new Map();
+
+export async function generateTestData() {
+  
+}
