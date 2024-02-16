889fe684eedc1ee9342eb6ba91412c83359b91f4
Andrew Jones <ajones@digitalbazaar.com>
2024-02-16 21:08:29 +0000

    Do not include ecdsa2019.

--- tests/vc-generator/cryptosuites.js
+++ tests/vc-generator/cryptosuites.js
@@ -5,14 +5,10 @@
 import * as ecdsaSd2023Cryptosuite from
   '@digitalbazaar/ecdsa-sd-2023-cryptosuite';
 import {
-  cryptosuite as ecdsa2019Cryptosuite
-} from '@digitalbazaar/ecdsa-2019-cryptosuite';
-import {
   cryptosuite as ecdsaRdfc2019Cryptosuite
 } from '@digitalbazaar/ecdsa-rdfc-2019-cryptosuite';
 
 export const cryptosuites = new Map([
-  ['ecdsa2019Cryptosuite', ecdsa2019Cryptosuite],
   ['ecdsa-rdfc-2019', ecdsaRdfc2019Cryptosuite],
   ['ecdsa-sd-2023', ecdsaSd2023Cryptosuite]
 ]);
