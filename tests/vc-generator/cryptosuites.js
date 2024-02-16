92035c4a27df60ca0219c5d8827cf177a303fbae
Andrew Jones <ajones@digitalbazaar.com>
2024-02-16 15:59:49 +0000

    Match tag to cryptosuite.

--- tests/vc-generator/cryptosuites.js
+++ tests/vc-generator/cryptosuites.js
@@ -2,10 +2,18 @@
  * Copyright 2023 Digital Bazaar, Inc.
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
+import * as ecdsaSd2023Cryptosuite from
+  '@digitalbazaar/ecdsa-sd-2023-cryptosuite';
+import {
+  cryptosuite as ecdsa2019Cryptosuite
+} from '@digitalbazaar/ecdsa-2019-cryptosuite';
+import {
+  cryptosuite as ecdsaRdfc2019Cryptosuite
+} from '@digitalbazaar/ecdsa-rdfc-2019-cryptosuite';
 
 export const cryptosuites = new Map([
-  ['ecdsa-rdfc-2019'],
-  ['ecdsa-sd-2023', ]
+  ['ecdsa2019Cryptosuite', ecdsa2019Cryptosuite],
+  ['ecdsa-rdfc-2019', ecdsaRdfc2019Cryptosuite],
+  ['ecdsa-sd-2023', ecdsaSd2023Cryptosuite]
 ]);
 
