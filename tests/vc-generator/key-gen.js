281275b8b2538544dba0020870cf6ee082f5eed1
Andrew Jones <ajones@digitalbazaar.com>
2024-02-20 20:16:36 +0000

    Add logic for key creation and basic issuance.

--- tests/vc-generator/key-gen.js
+++ tests/vc-generator/key-gen.js
@@ -2,19 +2,36 @@
  * Copyright 2023 Digital Bazaar, Inc.
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import * as Ecdsa25519Multikey from '@digitalbazaar/ecdsa-multikey';
+import * as EcdsaMultikey from '@digitalbazaar/ecdsa-multikey';
+import {require} from './helpers.js';
 
-export const getMultikey = async ({serializedKeyPair}) => {
-  if(!serializedKeyPair) {
-    throw new Error('serializedKeyPair required');
+const keys = require('../../config/keys.json');
+const formatKey = key => {
+  if(key.privateKeyMultibase && !key.secretKeyMultibase) {
+    key.secretKeyMultibase = key.privateKeyMultibase;
   }
-  const key = await Ecdsa25519Multikey.from(serializedKeyPair);
-  const signer = key.signer();
-  // The issuer needs to match the signer or the controller of the signer
-  const issuer = `did:key:${key.publicKeyMultibase}`;
-  // verificationMethod needs to be a fragment
-  // this only works for did:key
-  signer.id = `${issuer}#${key.publicKeyMultibase}`;
-  return {signer, issuer, key};
+  if(!key.controller) {
+    key.controller = `did:key:${key.publicKeyMultibase}`;
+  }
+  if(!key.id) {
+    key.id = `${key.controller}#${key.publicKeyMultibase}`;
+  }
+  return key;
 };
 
+export const getMultikeys = async () => {
+  const _keys = {};
+  for(const prop in keys) {
+    const serializedKeyPair = require(keys[prop]);
+    const formattedKey = formatKey(serializedKeyPair);
+    const key = await EcdsaMultikey.from(formattedKey);
+    const signer = key.signer();
+    // The issuer needs to match the signer or the controller of the signer
+    const issuer = `did:key:${key.publicKeyMultibase}`;
+    // verificationMethod needs to be a fragment
+    // this only works for did:key
+    signer.id = `${issuer}#${key.publicKeyMultibase}`;
+    _keys[prop] = {signer, issuer, key};
+  }
+  return _keys;
+};
