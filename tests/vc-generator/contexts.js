567fd0f687cfad8e337855dd29f4b0ead741282b
Andrew Jones <ajones@digitalbazaar.com>
2024-02-21 14:16:22 +0000

    Start documentLoader & contexts.

---
+++ tests/vc-generator/contexts.js
@@ -0,0 +1,32 @@
+/*!
+ * Copyright (c) 2022-2024 Digital Bazaar, Inc.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import credentialsCtx from 'credentials-context';
+import dataIntegrityCtx from '@digitalbazaar/data-integrity-context';
+import didCtx from '@digitalcredentials/did-context';
+import {klona} from 'klona';
+import multikeyCtx from '@digitalbazaar/multikey-context';
+
+const contextMap = new Map();
+
+const _dataIntegrityCtx = klona(dataIntegrityCtx.CONTEXT);
+
+// add contexts for the documentLoader
+contextMap.set(multikeyCtx.constants.CONTEXT_URL, multikeyCtx.CONTEXT);
+contextMap.set(
+  dataIntegrityCtx.constants.CONTEXT_URL,
+  _dataIntegrityCtx
+);
+contextMap.set(
+  didCtx.constants.DID_CONTEXT_URL,
+  didCtx.contexts.get(
+    didCtx.constants.DID_CONTEXT_URL)
+);
+contextMap.set(
+  credentialsCtx.constants.CONTEXT_URL,
+  credentialsCtx.contexts.get(
+    credentialsCtx.constants.CONTEXT_URL)
+);
+
+export {contextMap};
