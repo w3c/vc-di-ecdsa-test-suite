fc38e5f6f456045d688a1d553dfef6bc9404a156
Benjamin Young <byoung@digitalbazaar.com>
2024-03-28 14:18:03 -0400

    Switch README & .gitignore to use localConfig.cjs.

--- localConfig.example.cjs
+++ localConfig.example.cjs
@@ -1,4 +1,4 @@
-// Rename this file to .localImplementationsConfig.cjs
+// Rename this file to localConfig.cjs
 // you can specify a BASE_URL before running the tests such as:
 // BASE_URL=http://localhost:40443/zDdfsdfs npm test
 const baseUrl = process.env.BASE_URL || 'https://localhost:40443/id';
