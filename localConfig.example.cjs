deb240a326246c11e4c110456044b9b919d3cc9c
Benjamin Young <byoung@digitalbazaar.com>
2024-04-02 14:46:23 -0400

    Update localConfig.example.cjs to match latest.

--- localConfig.example.cjs
+++ localConfig.example.cjs
@@ -2,21 +2,22 @@
 // you can specify a BASE_URL before running the tests such as:
 // BASE_URL=http://localhost:40443/zDdfsdfs npm test
 const baseUrl = process.env.BASE_URL || 'https://localhost:40443/id';
-module.exports = [{
-  name: 'My Company',
-  implementation: 'My Implementation Name',
-  // only this implementation will be run in the suite
-  only: true,
-  issuers: [{
-    id: 'did:myMethod:implementation:issuer:id',
-    endpoint: `${baseUrl}/credentials/issue`,
-    supportedEcdsaKeyTypes: ['P-256', 'P-384'],
-    tags: ['ecdsa-rdfc-2019', 'localhost']
-  }],
-  verifiers: [{
-    id: 'did:myMethod:implementation:verifier:id',
-    endpoint: `${baseUrl}/credentials/verify`,
-    supportedEcdsaKeyTypes: ['P-256', 'P-384'],
-    tags: ['ecdsa-rdfc-2019', 'localhost']
-  }]
-}];
+module.exports = {
+  settings: {},
+  implementations: {
+    name: 'My Company',
+    implementation: 'My Implementation Name',
+    issuers: [{
+      id: 'did:myMethod:implementation:issuer:id',
+      endpoint: `${baseUrl}/credentials/issue`,
+      supportedEcdsaKeyTypes: ['P-256', 'P-384'],
+      tags: ['ecdsa-rdfc-2019']
+    }],
+    verifiers: [{
+      id: 'did:myMethod:implementation:verifier:id',
+      endpoint: `${baseUrl}/credentials/verify`,
+      supportedEcdsaKeyTypes: ['P-256', 'P-384'],
+      tags: ['ecdsa-rdfc-2019']
+    }]
+  }
+};
