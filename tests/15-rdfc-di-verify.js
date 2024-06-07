6c2811107867624e484decef90ab5294cf6a5f80
Andrew Jones <ajones@digitalbazaar.com>
2024-06-07 13:58:48 +0000

    Add basic vcVersion filter.

--- tests/15-rdfc-di-verify.js
+++ tests/15-rdfc-di-verify.js
@@ -13,15 +13,27 @@ import {getMultiKey} from './vc-generator/key-gen.js';
 import {getSuiteConfig} from './test-config.js';
 
 const {tags, vectors, credentials} = getSuiteConfig('ecdsa-rdfc-2019');
-// only use implementations with `ecdsa-rdfc-2019` verifiers.
-const {match} = endpoints.filterByTag({
-  tags: [...tags],
-  property: 'verifiers'
-});
 
 for(const vcVersion of vectors.vcTypes) {
   const key = await getMultiKey({keyType: 'P-256'});
   const {document} = credentials.verify[vcVersion];
+  // only use implementations with `ecdsa-rdfc-2019` verifiers.
+  // and that also support the current VC version
+  const {match} = endpoints.filter({
+    filter({implementation}) {
+      const endpoints = implementation.verifiers;
+      // the filter function expects an array to be returned
+      return endpoints.filter(e => {
+        // we want only endpoints that match every tag
+        if(tags.every(tag => e.tags.has(tag))) {
+          // only return endpoints with the vcVersion support
+          const {supports = {vc: ['1.1']}} = e?.settings;
+          return supports?.vc?.includes(vcVersion);
+        }
+        return false;
+      });
+    }
+  });
   // options for the DI Verifier Suite
   checkDataIntegrityProofVerifyErrors({
     implemented: match,
