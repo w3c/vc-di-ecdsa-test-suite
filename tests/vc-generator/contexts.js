3109a33b49f4c774af3fad93f865ebbbc56a1504
Andrew Jones <ajones@digitalbazaar.com>
2024-08-02 15:22:12 +0000

    Use release of VC 2.0 context of v2 context lib.

--- tests/vc-generator/contexts.js
+++ tests/vc-generator/contexts.js
@@ -2,14 +2,15 @@
  * Copyright (c) 2022-2024 Digital Bazaar, Inc.
  * SPDX-License-Identifier: BSD-3-Clause
  */
-import * as credentialsV2Context from '@digitalbazaar/credentials-v2-context';
-import credentialsCtx from 'credentials-context';
+import {
+  contexts as credentialsContexts
+} from '@digitalbazaar/credentials-context';
 import dataIntegrityCtx from '@digitalbazaar/data-integrity-context';
 import didCtx from '@digitalcredentials/did-context';
 import {klona} from 'klona';
 import multikeyCtx from '@digitalbazaar/multikey-context';
 
-const contextMap = new Map();
+const contextMap = new Map(credentialsContexts);
 
 // add contexts for the documentLoader
 contextMap.set(multikeyCtx.constants.CONTEXT_URL, multikeyCtx.CONTEXT);
@@ -21,17 +22,6 @@ contextMap.set(
   didCtx.constants.DID_CONTEXT_URL,
   klona(didCtx.contexts.get(
     didCtx.constants.DID_CONTEXT_URL))
-);
-contextMap.set(
-  credentialsCtx.constants.CONTEXT_URL,
-  klona(credentialsCtx.contexts.get(
-    credentialsCtx.constants.CONTEXT_URL))
-);
-
-contextMap.set(
-  credentialsV2Context.constants.CONTEXT_URL,
-  klona(credentialsV2Context.contexts.get(
-    credentialsV2Context.constants.CONTEXT_URL))
 );
 
 export {contextMap};
